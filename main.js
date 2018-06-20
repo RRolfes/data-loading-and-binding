f0f84b64f7804ec492cf9221be871fd0b3a72b50
Ryan Rolfes <ryanmrolfes@gmail.com>
2018-06-19 18:26:45 -0700

    formats code

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const data = [1,2,3,4,5];
 
 d3.select('body')
-.selectAll('p')
-.data(data)
-.enter()
-.append('p')
-.text('D3 is awesome!');
+  .selectAll('p')
+  .data(data)
+  .enter()
+  .append('p')
+  .text('D3 is awesome!');
